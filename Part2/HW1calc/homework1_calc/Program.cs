8838631dfa62e0aaea9f39482719da004bccc71b
takuumi <satu_noone@hotmail.com>
2019-08-07 00:15:12 +0900

    test

---
+++ Part2/HW1calc/homework1_calc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace homework1_calc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+            string parsedString = "hello abc";
+            var inputStream = new AntlrInputStream(parsedString);
+            var lexer = new calcLexer(inputStream);
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new calcParser(commonTokenStream);
+            var graphContext = parser.input();
+            Console.WriteLine(graphContext.ToStringTree());
+            
+
+        }
+    }
+}
