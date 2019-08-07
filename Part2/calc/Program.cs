387b31187e832e00945a85728dadddd812122b11
takuumi <satu_noone@hotmail.com>
2019-08-07 12:49:45 +0900

    プロジェクト追加

---
+++ Part2/calc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
