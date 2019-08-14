760ae390acc4fcc24c0311177466ec255c5813a5
takuumi <satu_noone@hotmail.com>
2019-08-14 23:04:53 +0900

    宿題２の環境整備中

---
+++ Part2/homework2_plcsim/plcsim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace plcsim
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
